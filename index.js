a6544562bffeaf789c49e912e22c95b8af9ee966
Thando <bukhosi95@gmail.com>
2021-12-26 16:50:06 +0000

    search in the title and body fields, hande document-slug searches

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 // const res = require('express/lib/response');
 // const { json } = require('express/lib/response');
 const app = express();
+let stopWord = require('stopword')
 
 const portNumber = 3000;
 app.use(express.json())
 // var mongo = require('mongodb');
-
 
 
 var MongoClient = require('mongodb').MongoClient;
@@ -34,23 +34,37 @@ app.post('/index',(request, response)=>{
 
 //search for a document
 app.get('/search', (req, res) => {
-     console.log(req.query.title)
+    console.log(req.query.words)
+    if(req.query.words === undefined) res.send('You need to specify some words')
+
+
+    //manually removing stop words.
+    //I dont need to remove stop words manually as MongoDB does this automatically, but I did this just for demonstration.
+    //Stop words list can be found here https://github.com/Mickael-van-der-Beek/mongo/commit/d4b38917b2314403642176c2ad6253a84b4e4c9f
+    
+    let oldString = req.query.words.trim().split(' ')
+    let removedStopWords = stopWord.removeStopwords(oldString)
+    console.log('==================')
+    console.log(removedStopWords)
+    console.log(removedStopWords.toString)
+    
     var theResponse;
     
     MongoClient.connect(url, function(err, db) {
         
-        if (err) throw err;
+        
+        if (err) throw res.send('Could not connect to server: ' + err.message);
             var dbo = db.db("documents");
             
             dbo.collection('cloud-ca').createIndex(
-                {title: 'text'}
+                {title: 'text', body: 'text'}
                 
             )
         
             console.log('===========MATCHED DOCUMENTS============');
 
             dbo.collection("cloud-ca")
-                .find({$text:{$search:req.query.title.toString()}}).toArray((err, data) => {
+                .find({$text:{$search:req.query.words.toString()}}).toArray((err, data) => {
                 if(err) throw err;
 
                 theResponse = data
@@ -61,12 +75,45 @@ app.get('/search', (req, res) => {
                 db.close();
 
             });
-        });
+    });
 })
 
+//return the mathching documents given its slugs
+app.get('/document-slug', (req, res) => {
 
-app.get('/document-slug', (request, response) => {
-    console.log('get to /document-slug')
+    let stringWithRemovedNumbers = req.query.words.replace(/[0-9]/g, '')
+    let stringWithRemovedHyphens = stringWithRemovedNumbers.replace(/-/g, ' ').trim()
+    let stringWithRemovedStopWords = stopWord.removeStopwords(stringWithRemovedHyphens.split(' ')).toString().replace(/,/g," ")
+
+
+    console.log(`unslugged-word: ${stringWithRemovedStopWords}`)
+    MongoClient.connect(url, function(err, db) {
+        
+        if (err) throw res.send('Could not connect to server: ' + err.message);
+            var dbo = db.db("documents");
+            
+            dbo.collection('cloud-ca').createIndex(
+                {title: 'text', body:'text'}
+                
+            )
+        
+            console.log('===========MATCHED DOCUMENTS============');
+
+            dbo.collection("cloud-ca")
+                .find({$text:{$search: stringWithRemovedHyphens}}).toArray((err, data) => {
+                if(err) throw err;
+
+                theResponse = data
+                
+                console.log(theResponse); 
+                res.send(data)
+            
+                db.close();
+
+            });
+    });
+
+
 })
 
 app.use((req, res) =>{
